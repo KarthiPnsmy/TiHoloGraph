083579db3c916d337c9f9dcf2d3b7587e3775a71
railskarthi <karthi.nkl@gmail.com>
2013-10-31 00:29:32 +0530

    initial commit

--- example/app.js
+++ example/app.js
@@ -1,3 +1,4 @@
+
 // This is a test harness for your module
 // You should do something interesting in this harness 
 // to test out the module and to provide instructions 
@@ -15,10 +16,14 @@ win.open();
 // TODO: write your module tests here
 var holograph = require('titutorial.holograph');
 Ti.API.info("module is => " + holograph);
-
+Ti.API.info("@@## module PIE_CHART => " + holograph.PIE_CHART);
+Ti.API.info("@@## module BAR_CHART => " + holograph.BAR_CHART);
+Ti.API.info("@@## module PIE_CHART1 => " + holograph.PIE_CHART1);
+Ti.API.info("@@## module BAR_CHART1 => " + holograph.BAR_CHART1);
 
 if (Ti.Platform.name == "android") {
 	var proxy = holograph.createExample({
+		chartType: "pie",
 		top: 100,
 		left: 20
 	});
